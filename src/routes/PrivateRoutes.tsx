560384477c092a410dde89c23f40a2eaa82c453f
Saif Maamun <sadifmamun@gmail.com>
2024-01-21 14:56:22 +0600

    navigate

---
+++ src/routes/PrivateRoutes.tsx
@@ -0,0 +1,22 @@
+import { useAppSelector } from '@/redux/hooks';
+import { ReactNode } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+
+interface IProps {
+  children: ReactNode;
+}
+
+const PrivateRoutes = ({ children }: IProps) => {
+  const { user, isLoading } = useAppSelector((state) => state.user);
+  const { pathname } = useLocation();
+  if (isLoading) {
+    <p>Loading...</p>;
+  }
+  if (!user.email && !isLoading) {
+    return <Navigate to="/login" state={{ path: pathname }}></Navigate>;
+  }
+
+  return children;
+};
+
+export default PrivateRoutes;
