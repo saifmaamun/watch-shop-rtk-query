6c3bababfa63520f4bce53b0017c0a7b8b09d3e8
Saif Maamun <sadifmamun@gmail.com>
2024-01-21 12:11:40 +0600

    async thunk

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,23 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: "AIzaSyBLFRRfTQRzZKQmvkTjBhfupbZyfTvpVrQ",
+  authDomain: "redux-shop-2b43d.firebaseapp.com",
+  projectId: "redux-shop-2b43d",
+  storageBucket: "redux-shop-2b43d.appspot.com",
+  messagingSenderId: "552173219182",
+  appId: "1:552173219182:web:07792343d1d6d2c0b2657f"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_apiKey,
+//   authDomain: import.meta.env.VITE_authDomain,
+//   projectId: import.meta.env.VITE_projectId,
+//   storageBucket: import.meta.env.VITE_storageBucket,
+//   messagingSenderId: import.meta.env.VITE_messagingSenderId,
+//   appId: import.meta.env.VITE_appId,
+// };
--- src/redux/features/user/userSlice.ts
+++ src/redux/features/user/userSlice.ts
@@ -1,5 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { auth } from "@/lib/firebase";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 interface IUserState{
  user:{
@@ -8,6 +10,10 @@ interface IUserState{
  isLoading:boolean,
  isError:boolean,
  error:string|null,
+}
+interface ICredential{
+    email:string ,
+    password:string,
 }
 
 const initialState:IUserState= {
@@ -18,12 +24,32 @@ const initialState:IUserState= {
    isError:false,
    error:null,
 }
+export const createUser = createAsyncThunk(
+    "user/create",
+    async({email,password}:ICredential)=>{
+        const data =await createUserWithEmailAndPassword(auth,email,password)
+    return data.user.email;
+    }
+)
 
 const userSlice=createSlice({
     name:"user",
     initialState,
     reducers:{
+    },
+    extraReducers:(builder)=>{
+        builder.addCase(createUser.pending,(state)=>{
+            state.isLoading = true;
+        }).addCase(createUser.rejected,(state)=>{
+            state.isLoading=false;
+            state.isError=true;
+
+        }).addCase(createUser.fulfilled,(state)=>{
+            state.isLoading=false;
+            state.isError=false;
+        })
     }
+    
 })
 
 export default userSlice.reducer;
